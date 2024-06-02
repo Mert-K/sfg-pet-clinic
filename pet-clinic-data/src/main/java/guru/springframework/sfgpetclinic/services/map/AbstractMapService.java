5a0f6fe6c66abe4fc780250cd8a2bb5250c80de8
Mert Kurt <tyrael91@hotmail.com>
2024-06-03 02:54:10 +0300

    Refactoring Map Impl to abstract ID generation. Closes #23

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,9 +1,11 @@
 package guru.springframework.sfgpetclinic.services.map;
 
+import guru.springframework.sfgpetclinic.model.BaseEntity;
+
 import java.util.*;
 
-public abstract class AbstractMapService<T, ID> {
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -13,8 +15,18 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+
+        if (object!=null){
+            if(object.getId() ==null){
+                object.setId(getNextId());
+            }
+
+            map.put(object.getId(),object);
+        }else{
+            throw new RuntimeException("Object cannot be null");
+        }
+
         return object;
     }
 
@@ -25,4 +37,15 @@ public abstract class AbstractMapService<T, ID> {
     void deleteById(ID id) {
         map.remove(id);
     }
+
+    private Long getNextId(){
+        Long nextId = null;
+        try{
+            nextId= Collections.max(map.keySet()) +1;
+        }catch (NoSuchElementException ex){
+            nextId = 1L;
+        }
+        return nextId;
+    }
+
 }
